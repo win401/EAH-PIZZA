b72404ee9dc5d167e6e0cf66f4598dda51101a1c
win401 <winnerv401@gmail.com>
2024-03-04 14:50:59 +0900

    fix path /eah-pizza

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -14,15 +14,14 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        <Route element={<Layout />}>
+          <Route index path="/eah-pizza" element={<Home />} />
           <Route path="Menu" element={<Menu />} />
           <Route path="Location" element={<Location />} />
           <Route path="Contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="/*" element={<NoPage />} />
         </Route>
       </Routes>
-      <Home />
     </BrowserRouter>
   );
 };
