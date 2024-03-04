af154b15a548caabffc2af7f844f0c3a85c18be6
win401 <winnerv401@gmail.com>
2024-03-04 16:02:50 +0900

    add useLocation

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,24 +1,25 @@
-// Layout.js
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import "../styles/Layout.css";
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
       <nav>
         <p>Everyday’s another holiday</p>
         <ul>
-          <li>
+          <li className={`myElement ${location.pathname === "/" ? "active" : ""}`}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={`myElement ${location.pathname === "/Menu" ? "active" : ""}`}>
             <Link to="/Menu">Menu</Link>
           </li>
-          <li>
+          <li className={`myElement ${location.pathname === "/Location" ? "active" : ""}`}>
             <Link to="/Location">Location</Link>
           </li>
-          <li>
+          <li className={`myElement ${location.pathname === "/Contact" ? "active" : ""}`}>
             <Link to="/Contact">Contact</Link>
           </li>
         </ul>
